95a3e8103c03c5245e6678860dbee6ba6af6ae37
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-25 14:03:18 +0600

    Update main.js

--- express js/main.js
+++ express js/main.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const app = express()
-app.get("/",function(request,response){
+app.get("/",function(req,res){  //route is "/" which also called the home route
     //console.log(request)
-    response.send("Whats up")
+    res.send("<h1>Hello babe</h1>")  
 })
-
+app.get("/gg",function(req,res){
+    res.send("<em>019.........</em>")
+})
+app.get("/me",function(req,res){
+    res.send("My name is MD Ikramul Kayes")
+})
 app.listen(3000,function(){
     console.log("Server Started")
 })
